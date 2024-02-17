a1a7d31105f43bc08774a51171c7f5e7ffb56a42
Wantedbear007 <pratapsinghbhanu444@gmail.com>
2024-02-17 23:40:16 +0530

    added CRUD operations.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import {
+  GetObjectCommand,
+  S3Client,
+  PutObjectCommand,
+  ListObjectsV2Command,
+  DeleteObjectCommand  
+} from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import dotenv from "dotenv";
 
@@ -19,7 +25,7 @@ const s3Client = new S3Client({
 
 // to get object
 async function getImageUrl(key: string, bucket: string): Promise<string> {
-  const command = new GetObjectCommand({
+  const command: GetObjectCommand = new GetObjectCommand({
     Bucket: bucket,
     Key: key,
   });
@@ -28,11 +34,55 @@ async function getImageUrl(key: string, bucket: string): Promise<string> {
   return url;
 }
 
-async function init(): Promise<void> {
-  console.log(
-    "URL is: ",
-    await getImageUrl("bhanuImage.jpg", "wantedbear007-private")
-  );
+// to put object in aws
+async function putObject(
+  bucket: string,
+  fileName: string,
+  contentType: string
+): Promise<string> {
+  const command: PutObjectCommand = new PutObjectCommand({
+    Bucket: bucket,
+    Key: `/uploads/user-files/${fileName}`,
+    ContentType: contentType,
+  });
+
+  const url: string = await getSignedUrl(s3Client, command, {expiresIn: 2000});
+
+  return url;
+}  
+
+// to list objects
+async function listObjects(): Promise<void> {
+  const command: ListObjectsV2Command = new ListObjectsV2Command({
+    Bucket: "wantedbear007-private",
+    
+  })
+
+  const res: any = await s3Client.send(command)
+
+  console.log(res)
 }
 
-// init();
+// to delete objects
+async function deleteObject(): Promise<void> {
+  const command = new DeleteObjectCommand({
+    Bucket: "wantedbear007-private",
+    Key: "/uploads/user-files/profilePic.jpeg"
+
+  })
+
+  await s3Client.send(command);
+  
+}
+
+async function init(): Promise<void> {
+    // await deleteObject();
+  // await listObjects();
+  // console.log(
+  //   "URL is: ",
+  //   await getImageUrl("bhanuImage.jpg", "wantedbear007-private")
+  // );
+  // console.log("url to add file: ", await putObject("wantedbear007-private", "profilePic.jpeg", "image/jpeg"))
+}
+
+init();
